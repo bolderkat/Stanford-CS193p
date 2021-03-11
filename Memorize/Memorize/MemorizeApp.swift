8b22d5ac5c7b3e387fecd581792a462c9e8a9f18
Daniel Luo <d.luo117@gmail.com>
2021-03-10 17:40:24 -0800

    Memorize initial setup

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Luo on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
