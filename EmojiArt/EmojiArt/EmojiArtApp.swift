96d5bb71512110623de4427fe3fe4a46d2ef331d
Daniel Luo <d.luo117@gmail.com>
2021-04-08 12:12:16 -0700

    EmojiArt project initial setup

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Daniel Luo on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
